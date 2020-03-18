99df0b06017a228812fb455a5ccfcfc7c9a56171
RayanAdmin <50583793+RayanAdmin@users.noreply.github.com>
2020-03-18 22:32:36 +0100

    Create variables.tf

---
+++ DeployInfra/modules/aws-vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "network-address" {
+  description = "Adresse du Reseau (format CICR)"
+  type = string
+  default = {}
+}
+
+variable "dns_support" {
+  description = "activer DNS Support"
+  type = string
+  default = {true}
+}
+
+variable "dns_hostnames" {
+  description = "activer resolution DNS"
+  type = string
+  default = {true}
+}
+
+
