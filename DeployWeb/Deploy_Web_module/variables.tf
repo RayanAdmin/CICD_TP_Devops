adebc3103354f61bb4df1b4512fac8fa021492bc
RayanAdmin <50583793+RayanAdmin@users.noreply.github.com>
2020-03-18 16:37:48 +0100

    Update variables.tf

--- DeployWeb/Deploy_Web_module/variables.tf
+++ DeployWeb/Deploy_Web_module/variables.tf
@@ -1,17 +1,10 @@
-variable "subnet_ids" {
-    description = "The list of subnet id"
+
+variable "env" {
+  type    = string
+  default = "dev"
 }
 
-variable "cluster_size" {
-    description "The number of instance you want"
-    default = 1
-}
-
-variable "instance_type" {
-    description = "The type of instance you want"
-    default = "t2.micro"
-}
-
-variable "ami_id" {
-    description = "The AMI to use on these instances"
+variable "app_name" {
+  type    = string
+  default = "WebApache"
 }
