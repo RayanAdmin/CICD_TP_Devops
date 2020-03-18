3372c777a6cec6a96ac47b91a32b61db15f6d49d
RayanAdmin <50583793+RayanAdmin@users.noreply.github.com>
2020-03-18 21:47:15 +0100

    Update infra.tf

--- DeployInfra/infra.tf
+++ DeployInfra/infra.tf
@@ -12,18 +12,6 @@ terraform {
   }
 }
 
-# VPC
-resource "aws_vpc" "vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = "true"
-  enable_dns_hostnames = "true"
-  enable_classiclink   = "false"
-  instance_tenancy     = "default"
-
-  tags = {
-    Name = "${var.env}-vpc"
-  }
-}
 
 # IGW
 resource "aws_internet_gateway" "igw" {
