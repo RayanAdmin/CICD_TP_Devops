671f898720261f524279e0c6302df8a763685340
RayanAdmin <50583793+RayanAdmin@users.noreply.github.com>
2020-03-18 21:45:36 +0100

    Update maint.tf

--- DeployInfra/modules/aws-vpc/maint.tf
+++ DeployInfra/modules/aws-vpc/maint.tf
@@ -1,1 +1,12 @@
-test
+# VPC
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  enable_classiclink   = "false"
+  instance_tenancy     = "default"
+
+  tags = {
+    Name = "${var.env}-vpc"
+  }
+}
