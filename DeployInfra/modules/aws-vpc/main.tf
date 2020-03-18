fbf6b7628b7240d7f3258a76dd88fc5adab5801e
RayanAdmin <50583793+RayanAdmin@users.noreply.github.com>
2020-03-18 23:25:28 +0100

    Update main.tf

--- DeployInfra/modules/aws-vpc/main.tf
+++ DeployInfra/modules/aws-vpc/main.tf
@@ -13,14 +13,14 @@ resource "aws_vpc" "vpc" {
 }
 
 
-output "aws_vpc.vpc.arn" {
+output "aws_vpc_arn" {
   description = "ARN of the VPC"
   value       = aws_vpc.vpc.arn
 }
 
 
 
-output "aws_vpc.vpc.id" {
+output "aws_vpc_id" {
   description = "Name (id) of the VPC"
   value       = aws_vpc.vpc.id
 }
