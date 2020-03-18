796b3bdca7b31cf81e0f7a7b545c0a8182805dd4
RayanAdmin <50583793+RayanAdmin@users.noreply.github.com>
2020-03-19 00:27:57 +0100

    Update main.tf

--- DeployInfra/modules/aws-vpc/main.tf
+++ DeployInfra/modules/aws-vpc/main.tf
@@ -4,8 +4,8 @@ resource "aws_vpc" "vpc" {
   cidr_block           = var.network_address
   enable_dns_support   = var.dns_support
   enable_dns_hostnames = var.dns_hostnames
-  enable_classiclink   = false
-  instance_tenancy     = default
+  enable_classiclink   = "false"
+  instance_tenancy     = "default"
 
   tags = {
     Name = var.name_vpc
