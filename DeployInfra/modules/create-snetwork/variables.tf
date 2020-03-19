f23ce528b8bc3d8240313a287bf389bd60785e06
RayanAdmin <50583793+RayanAdmin@users.noreply.github.com>
2020-03-19 11:11:11 +0100

    Update variables.tf

--- DeployInfra/modules/create-snetwork/variables.tf
+++ DeployInfra/modules/create-snetwork/variables.tf
@@ -1,19 +1,30 @@
-variable "network_address" {
-  description = "Adresse du Reseau (format CICR)"
+variable "subnet_name" {
+  description = "Nom du subnet"
   type = string
 }
 
-variable "dns_support" {
+variable "vpc_id" {
   description = "activer DNS Support"
   type = string
 }
 
-variable "dns_hostnames" {
-  description = "activer resolution DNS"
+variable "cidr_block" {
+  description = "cidr_block"
   type = string
 }
 
-variable "name_vpc" {
-  description = "nom du vpc"
+variable "map_public_ip_on_launch" {
+  description = "map_public_ip_on_launch"
   type = string
+}
+
+variable "availability_zone" {
+  description = "map_public_ip_on_launch"
+  type = string
+}
+
+variable "tags" {
+  description = "tags"
+  type = map(string)
+  default = {}
 }
