60d04354dae3247973ab6afa4164ed2d54bf8d08
RayanAdmin <50583793+RayanAdmin@users.noreply.github.com>
2020-03-19 10:56:26 +0100

    Update main.tf

--- DeployInfra/modules/create-snetwork/main.tf
+++ DeployInfra/modules/create-snetwork/main.tf
@@ -1,69 +1,11 @@
+#Subnet
 
-# Subnets
-## Public
-### AZ1
-resource "aws_subnet" "subnet-public-1" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-3a"
+resource "aws_subnet" var.subnet_name {
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.cidr_block
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+  availability_zone       = var.availability_zone
   tags = {
-    Name = "${var.env}-subnet-public-1"
-  }
-}
-
-### AZ2
-resource "aws_subnet" "subnet-public-2" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-3b"
-  tags = {
-    Name = "${var.env}-subnet-public-2"
-  }
-}
-
-### AZ3
-resource "aws_subnet" "subnet-public-3" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.3.0/24"
-  map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-3c"
-  tags = {
-    Name = "${var.env}-subnet-public-3"
-  }
-}
-
-## Private
-### AZ1
-resource "aws_subnet" "subnet-private-1" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.4.0/24"
-  map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-3a"
-  tags = {
-    Name = "${var.env}-subnet-private-1"
-  }
-}
-
-### AZ2
-resource "aws_subnet" "subnet-private-2" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.5.0/24"
-  map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-3b"
-  tags = {
-    Name = "${var.env}-subnet-private-2"
-  }
-}
-
-### AZ3
-resource "aws_subnet" "subnet-private-3" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.6.0/24"
-  map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-3c"
-  tags = {
-    Name = "${var.env}-subnet-private-3"
+    Name = var.tags
   }
 }
