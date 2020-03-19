90280aa0cf585944043b3fed62807538cb0a53ba
RayanAdmin <50583793+RayanAdmin@users.noreply.github.com>
2020-03-19 01:40:48 +0100

    Update outputs.tf

--- DeployInfra/modules/aws-vpc/outputs.tf
+++ DeployInfra/modules/aws-vpc/outputs.tf
@@ -13,6 +13,13 @@ output "aws_vpc_id" {
 
 
 
+output "id" {
+  description = "Name (id) of the VPC"
+  value       = aws_vpc.vpc.id
+}
+
+
+
 
 output "route_table" {
   description = "Route-table of the VPC"
