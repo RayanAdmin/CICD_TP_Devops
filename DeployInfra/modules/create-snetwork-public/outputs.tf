d2faa94e2ad184d2f7df010c3c387bf765f6f02f
RayanAdmin <50583793+RayanAdmin@users.noreply.github.com>
2020-03-19 12:05:10 +0100

    Update outputs.tf

--- DeployInfra/modules/create-snetwork-public/outputs.tf
+++ DeployInfra/modules/create-snetwork-public/outputs.tf
@@ -1,30 +1,17 @@
 output "arn" {
   description = "ARN of the VPC"
-  value       = aws_vpc.vpc.arn
+  value       = aws_subnet.${var.subnet_name}.arn
 }
 
 
 
-output "aws_vpc_id" {
+output "aws_subnet_id" {
   description = "Name (id) of the VPC"
-  value       = aws_vpc.vpc.id
+  value       = aws_subnet.${var.subnet_name}.id
 }
 
 
 output "id" {
   description = "Name (id) of the VPC"
-  value       = aws_vpc.vpc.id
-}
-
-
-output "aws_route_table" {
-  description = "Route-table of the VPC"
-  value       = aws_vpc.vpc.default_route_table_id
-}
-
-
-
-output "default_route_table_id" {
-  description = "Route-table of the VPC"
-  value       = aws_vpc.vpc.default_route_table_id
+  value       = aws_subnet.${var.subnet_name}.id
 }
