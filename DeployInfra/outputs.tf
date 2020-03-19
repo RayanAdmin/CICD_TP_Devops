460af077229bb6fd16410810d936de282e86b6d3
RayanAdmin <50583793+RayanAdmin@users.noreply.github.com>
2020-03-19 11:23:50 +0100

    Update outputs.tf

--- DeployInfra/outputs.tf
+++ DeployInfra/outputs.tf
@@ -12,3 +12,17 @@ output "aws_route_table" {
   description = "Route-table of the VPC"
   value       = module.create_vpc.aws_route_table
 }
+
+for (i = 1; i < 4; i++) {
+output "aws_subnet_subnet_public_${i}" {
+  description = "Name (id) of the VPC"
+  value       = module.create_snetwork.${i}.id
+ }
+}
+
+for (i = 4; i < 7; i++) {
+output "aws_subnet_subnet_private_${i}" {
+  description = "Name (id) of the VPC"
+  value       = module.create_snetwork.${i}.id
+ }
+}
