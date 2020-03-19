cc78c92fcc24c1c6d56d9d974a0f3bc95eb5f496
RayanAdmin <50583793+RayanAdmin@users.noreply.github.com>
2020-03-19 11:45:48 +0100

    Update main.tf

--- DeployInfra/modules/create-snetwork-public/main.tf
+++ DeployInfra/modules/create-snetwork-public/main.tf
@@ -1,1 +1,11 @@
+#Subnet
 
+resource "aws_subnet" var.subnet_name {
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.cidr_block
+  map_public_ip_on_launch = "true"
+  availability_zone       = var.availability_zone
+  tags = {
+    Name = var.tags
+  }
+}
